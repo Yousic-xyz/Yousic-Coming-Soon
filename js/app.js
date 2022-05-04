4c14d61fdda49dbc90e4ebdac1fab9e943602f02
Colin Breeding <colinmbreeding@gmail.com>
2022-05-04 17:29:29 -0400

    update nav bar

--- js/app.js
+++ js/app.js
@@ -5,3 +5,25 @@ function scrollHeader() {
     if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+
+// SHOW MENU
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+// MENU SHOW
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+    })
+}
+
+// MENU HIDDEN
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+    })
+}
